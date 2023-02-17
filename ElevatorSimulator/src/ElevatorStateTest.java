aa03f7bc9d877f4e9a21aec88e20d2e6b60df0a1
Mohammed-khair <mrdk3002@gmail.com>
2023-02-17 03:36:09 +0300

    created unit tests for ElevatorStateTest.java

---
+++ ElevatorSimulator/src/ElevatorStateTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+public class ElevatorStateTest {
+
+    @org.junit.Test
+    public void testConstructorandGetters() {
+        ElevatorState state = new ElevatorState();
+        assertEquals(1, state.getFloorLevel());
+        assertTrue(state.isIdleStatus());
+        assertEquals(Direction.UP , state.getDirection());
+    }
+
+
+
+    @org.junit.Test
+    public void setDirection() {
+        ElevatorState state = new ElevatorState();
+        state.setDirection(Direction.DOWN);
+        assertEquals(Direction.DOWN , state.getDirection());
+    }
+
+    @org.junit.Test
+    public void setFloorLevel() {
+        ElevatorState state = new ElevatorState();
+        state.setFloorLevel(4);
+        assertEquals(4 , state.getFloorLevel());
+    }
+
+    @org.junit.Test
+    public void setIdleStatus() {
+        ElevatorState state = new ElevatorState();
+        state.setIdleStatus(false);
+        assertFalse(state.isIdleStatus());
+    }
+}
