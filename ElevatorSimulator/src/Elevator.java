576cf55722faac8c8c4955498bb5d9c18892933f
AliEl-Khatib <elkhatib.aliwalid@gmail.com>
2023-02-26 18:42:24 -0500

    Updated Test for elevator, and hopefully fixed some issues

--- ElevatorSimulator/src/Elevator.java
+++ ElevatorSimulator/src/Elevator.java
@@ -2,7 +2,7 @@
  * The Elevator class represents the subsystem of
  * elevators in the simulaton. It only is meant for
  * 1 elevator for iteration 1
- * 
+ *
  * @author Ali El-Khatib
  * @version 1.0
  */
@@ -25,7 +25,7 @@ public class Elevator implements Runnable{
         this.scheduler = scheduler;
         this.id = id;
         state = new ElevatorState();
-        command = null:
+        command = null;
     }
 
     /**
@@ -37,26 +37,15 @@ public class Elevator implements Runnable{
     public void run() {
 
         while (!scheduler.shouldExit()) {
-            //Command command = scheduler.getCommand();
-            //commands.add(command);
+            Command command = this.getCommand();
 
             //Checks whether the elevator should go up or down
-            if (state.getIdleStatus()) {
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                }
-
-                //Checks if a command is serviced, and removes it if it is serviced
-                if (state.getFloorLevel() == command.getFloor()) {
-                    System.out.println("Elevator finished Command:\n" + command + "\n");
-                    scheduler.placeServicedCommand(command);
-                    //command = null;
-                    state.setIdleStatus(true);
-                }
-
-                moveFloor(); //Moves floor based on idle status and direction
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
             }
+
+            moveFloor(command); //Moves floor based on idle status and direction
 
         }
     }
@@ -65,24 +54,33 @@ public class Elevator implements Runnable{
      * Getter method for the state of elevator
      * @return the ElevatorState of the elevator
      */
-    public ElevatorState getState() {
+    public synchronized ElevatorState getState() {
         return state;
     }
 
     /**
-     * Adds a command to the commands list
-     * @param command the command that will be added to commands
+     * Puts the command into the command field. Also,
+     * the method sets up direction and sets idle status to false
+     * @param command the command that will be put into command
      */
     public synchronized void putCommand(Command command){
+        while (!state.isIdleStatus()){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
         this.command = command;
         System.out.println("Elevator received Command:\n" + command + "\n");
-        state.setIdleStatus(false);
+
+        //Determine which direction to go by comparing the state and command
         if (state.getFloorLevel() > command.getFloor()) {
             state.setDirection(Direction.DOWN);
         }
         else{
-            state.setDirection(Direction.UP)
+            state.setDirection(Direction.UP);
         }
+        state.setIdleStatus(false);
         notifyAll();
     }
 
@@ -91,8 +89,8 @@ public class Elevator implements Runnable{
      * @return the command from the elevator
      */
     public synchronized Command getCommand(){
-        state.setIdleStatus(true);
-        while (state.getIdleStatus()){
+
+        while (state.isIdleStatus()){
             try {
                 wait();
             } catch (InterruptedException e) {
@@ -107,17 +105,21 @@ public class Elevator implements Runnable{
     /**
      * Moves the elevator up or down based on the direction and idle
      * status of the elevator
+     * @param command the Command to execute
      */
-    private void moveFloor(){
+    private synchronized void moveFloor(Command command){
         // return if the elevator is idle.
-        if (state.getIdleStatus())
+        if (state.isIdleStatus())
             return;
-        if (state.getDirection() == Direction.UP) {
-            state.goUp(); //Increments the current floor level of the elevator by 1.
-        } else {
-            state.goDown(); //Decrements the current floor level of the elevator by 1.
+        state.setFloorLevel(command.getFloor());
+        System.out.println("Elevator is now on floor: " + state.getFloorLevel() + "\n");
+
+        //Check if elevator floor and command floor are equal
+        if (state.getFloorLevel() == command.getFloor()) {
+            System.out.println("Elevator finished Command:\n" + command + "\n");
+            state.setIdleStatus(true); //Set idle status to true since the command is done
         }
-        System.out.println("Elevator is now on floor: " + state.getFloorLevel()));
+        notifyAll();
     }
 
 }
--- ElevatorSimulator/test/ElevatorTest.java
+++ ElevatorSimulator/test/ElevatorTest.java
@@ -1,111 +1,128 @@
-// Tracking command line output
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-// The usual junit imports
-import org.junit.Assert;
-
-import org.junit.Before;
-import org.junit.After;
-import org.junit.Test;
-
-// Mockito imports to isolate Elevator
-import static org.mockito.Mockito.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.Answer;
-
 /**
- * Tests for the Elevator class.
- * 
- * @author Ethan Leir 101146422
+ * The Elevator class represents the subsystem of
+ * elevators in the simulaton. It only is meant for
+ * 1 elevator for iteration 1
+ *
+ * @author Ethan Leir
  * @version 1.0
+ *
+ * @author Ali El-Khatib
+ * @version 2.0
  */
-@ExtendWith(MockitoExtension.class)
-public class ElevatorTest {
-	private final ByteArrayOutputStream commandlineOutput = new ByteArrayOutputStream();
-	private final PrintStream originalOutput = System.out;
-	
+public class Elevator implements Runnable{
+
+	private Scheduler scheduler; //Represents the shared scheduler between the elevator and the floor
+	private int id; //Represents the id of the elevator
+
+	private ElevatorState state; //state of the elevator
+
 	private Command command;
-	private Elevator elevator;
-	private Scheduler schedulerMock;
-	
+
 	/**
-	 * Sets up the mock object for Scheduler which will:
-	 * return this.command on a call to getCommand()
-	 * return false on the first call to shouldExit(), and true otherwise.
-	 * Creates an Elevator object with the mock scheduler as an dependency.
+	 * Constructs and elevator using a scheduler and id
+	 *
+	 * @param scheduler the shared scheduler between the elevator and the floor
+	 * @param id the id of the elevator
 	 */
-	@Before
-	public void setupMocks() {
-		this.command = new Command(5, 2, Direction.UP, 9);
-		
-		schedulerMock = mock(Scheduler.class);
-		when(schedulerMock.getCommand()).thenReturn(this.command);
-		when(schedulerMock.shouldExit()).thenAnswer(
-			     new Answer() {
-			    	 private int n = 0;
-			         public Object answer(InvocationOnMock invocation) {
-			             Object[] args = invocation.getArguments();
-			             Object mock = invocation.getMock();
-			             
-			             boolean ret = false;
-			             if (n > 0) {
-			            	 ret = true;
-			             }
-			             ++n;
-			             
-			             return ret;
-			         }
-			 });
-		
-		this.elevator = new Elevator(schedulerMock, 42);
+	public Elevator(Scheduler scheduler, int id) {
+		this.scheduler = scheduler;
+		this.id = id;
+		state = new ElevatorState();
+		command = null;
 	}
-	
+
 	/**
-	 * Sets the System.out stream to one that can be tracked by the test.
+	 * Continuosly gets commands from the scheduler until
+	 * there are no more commands and the floor is done
+	 * reading the file
 	 */
-	@Before
-	public void setupStreams() {
-		System.setOut(new PrintStream(commandlineOutput));
+	//@Override
+	public void run() {
+
+		while (!scheduler.shouldExit()) {
+			Command command = this.getCommand();
+
+			//Checks whether the elevator should go up or down
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+			}
+
+			moveFloor(command); //Moves floor based on idle status and direction
+
+		}
 	}
-	
+
 	/**
-	 * Sets the System.out stream back to the original.
+	 * Getter method for the state of elevator
+	 * @return the ElevatorState of the elevator
 	 */
-	@After
-	public void restoreStreams() {
-	    System.setOut(originalOutput);
+	public synchronized ElevatorState getState() {
+		return state;
 	}
-	
+
 	/**
-	 * Test for the Elevator.run() method.
-	 * Asserts that the correct message is printed.
-	 * Asserts that the elevator calls Scheduler.getCommand(),
-	 * Scheduler.placeServicedCommand(), and Scheduler.shouldExit()
-	 * the correct number of times when only one command is provided.
+	 * Puts the command into the command field. Also,
+	 * the method sets up direction and sets idle status to false
+	 * @param command the command that will be put into command
 	 */
-	@Test
-	public void testRun() {
-		elevator.run();
-		
-		String expected = "Elevator received Command:\n"
-				+ command
-				+ "\n\r\n"
-				+ "Elevator finished Command:\n"
-				+ command;
-		
-		expected = expected.trim();
-		String actual = commandlineOutput.toString().trim();
-		Assert.assertEquals(
-				expected,
-				actual
-				);
-		
-		verify(schedulerMock, times(1)).getCommand();
-		verify(schedulerMock, times(1)).placeServicedCommand(command);
-		verify(schedulerMock, times(2)).shouldExit();
+	public synchronized void putCommand(Command command){
+		while (!state.isIdleStatus()){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		this.command = command;
+		System.out.println("Elevator received Command:\n" + command + "\n");
+
+		//Determine which direction to go by comparing the state and command
+		if (state.getFloorLevel() > command.getFloor()) {
+			state.setDirection(Direction.DOWN);
+		}
+		else{
+			state.setDirection(Direction.UP);
+		}
+		state.setIdleStatus(false);
+		notifyAll();
+	}
+
+	/**
+	 * Gets the command from the elevator
+	 * @return the command from the elevator
+	 */
+	public synchronized Command getCommand(){
+
+		while (state.isIdleStatus()){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		Command temp = command;
+		command = null;
+		return temp;
+	}
+
+
+	/**
+	 * Moves the elevator up or down based on the direction and idle
+	 * status of the elevator
+	 * @param command the Command to execute
+	 */
+	private synchronized void moveFloor(Command command){
+		// return if the elevator is idle.
+		if (state.isIdleStatus())
+			return;
+		state.setFloorLevel(command.getFloor());
+		System.out.println("Elevator is now on floor: " + state.getFloorLevel() + "\n");
+
+		//Check if elevator floor and command floor are equal
+		if (state.getFloorLevel() == command.getFloor()) {
+			System.out.println("Elevator finished Command:\n" + command + "\n");
+			state.setIdleStatus(true); //Set idle status to true since the command is done
+		}
+		notifyAll();
 	}
 
 }
