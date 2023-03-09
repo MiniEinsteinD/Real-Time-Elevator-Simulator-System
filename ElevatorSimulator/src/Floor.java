b689c3f9ed592f5b51bccdaa5c1d1241ad5a357b
ethanleir <eleir9267@gmail.com>
2023-03-08 22:50:12 -0500

    Floor subsystem as a process.

--- ElevatorSimulator/src/Floor.java
+++ ElevatorSimulator/src/Floor.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.net.*;
 
 /**
  * The Floor class represents the floor subsystem
@@ -8,11 +9,12 @@ import java.util.ArrayList;
  * @version 01
  */
 
- public class Floor implements Runnable{
+ public class Floor {
     private File file; // File stores the commands that needs to be processed
-    private boolean ordersFinished; // boolean expression that represent that the command finished executing
-    private Scheduler scheduler; // The shared scheduler between the elevator and the floor
     private ArrayList<Command> commandList; //List of all commands found in the text file
+    private DatagramSocket sendReceiveSocket;
+    private DatagramPacket sendPacket;
+    private DatagramPacket receivePacket;
 
     /**
      * Constructs a floor using the scheduler and a file
@@ -21,9 +23,16 @@ import java.util.ArrayList;
      */
     public Floor(Scheduler scheduler,File file){
         this.file =  file;
-        this.ordersFinished = false;
-        this.scheduler = scheduler;
         commandList = new ArrayList<>();
+        // Networking
+        try {
+            sendReceiveSocket = new DatagramSocket();
+            sendPacket = null;
+            receivePacket = null;
+        } catch (SocketException se) {
+            sendReceiveSocket.close();
+            throw new RuntimeException(se);
+        }
     }
 
     /**
@@ -31,8 +40,7 @@ import java.util.ArrayList;
      * Continuously sends commands and receives responses from the scheduler until
      * there are no more commands
      */
-    //@Override
-    public void run() {
+    public void startSubsystem() {
 
         // read lines from file and store them as strings in an ArrayList
         BufferedReader bufReader = null;
@@ -57,6 +65,9 @@ import java.util.ArrayList;
             try {
                 line = bufReader.readLine();
             } catch (IOException e) {
+                try {
+                    bufReader.close();
+                } catch (IOException ioe) {}
                 throw new RuntimeException(e);
             }
         }
@@ -72,10 +83,33 @@ import java.util.ArrayList;
             commandList.add(new Command(command));
         }
         //send all the commands to the scheduler
-        scheduler.placeCommandList(commandList);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(commandList);
+            oos.flush();
+            byte[] data = bos.toByteArray();
 
-        //notify scheduler that there is no more commands to execute
-        scheduler.exitThreads();
+            // Send data to SchedulerReciever
+            sendPacket = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), 23);
+            System.out.println("Floor: Sending commands to Scheduler subsystem.");
+            sendReceiveSocket.send(sendPacket);
+
+            // Recieve confirmation from SchedulerReciever.
+            byte[] response = new byte[100];
+            receivePacket = new DatagramPacket(response, response.length);
+            System.out.println("Floor: Receiving confirmation of completion from the Scheduler subsystem.");
+            sendReceiveSocket.receive(receivePacket);
+
+            // In the future we'll handle commands arriving at different times here.
+
+        } catch (IOException ioe) {} finally {
+            // Cleanup
+            try {
+                bos.close();
+            } catch (IOException ioe) {}
+            sendReceiveSocket.close();
+        }
     }
 
 }
