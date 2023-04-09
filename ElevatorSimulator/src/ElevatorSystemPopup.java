adca962b3636dcb13279e1c536268ba9ba773be9
Mohammed-khair <mrdk3002@gmail.com>
2023-04-09 21:05:05 +0300

    changed the max number of floors to 25

--- ElevatorSimulator/src/ElevatorSystemPopup.java
+++ ElevatorSimulator/src/ElevatorSystemPopup.java
@@ -42,7 +42,7 @@ public class ElevatorSystemPopup extends JFrame {
         panel.add(elevatorsPanel);
 
         JLabel floorsLabel = new JLabel("Number of Floors:");
-        floorsComboBox = new JComboBox<>(new Integer[]{5, 6, 7, 8, 9, 10});
+        floorsComboBox = new JComboBox<>(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25});
         //add floorsComboBox to panel
         JPanel floorsPanel = new JPanel();
         floorsPanel.add(floorsLabel);
